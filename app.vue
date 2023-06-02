e33ae97936bb246f9c4465fba9c9bc809ca23594
Neha Mohan <neha@Nehas-MacBook-Pro.local>
2023-06-02 13:44:18 +0200

    First commit

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+<!--    <NuxtWelcome />-->
+    <NuxtPage />
+    hola
+  </div>
+
+
+</template>
+<script setup>
+const route = useRoute()
+if (route.params.group === 'admins' && !route.params.id) {
+  console.log('Warning! Make sure user is authenticated!')
+}
+</script>
